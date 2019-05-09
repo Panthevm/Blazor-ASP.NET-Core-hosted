45d57f842f02477c59f776cb9d643d6f1ce3873d
Panthevm <43318093+Panthevm@users.noreply.github.com>
2019-05-09 13:45:16 +0300

    Fix Navbar Dropdown menu

--- Dekanat.Client/Startup.cs
+++ Dekanat.Client/Startup.cs
@@ -6,7 +6,6 @@ namespace Dekanat.Client {
     public class Startup {
         public void ConfigureServices(IServiceCollection services) {
         }
-        int test;
         public void Configure(IComponentsApplicationBuilder app) {
             app.AddComponent<App>("app");
         }
