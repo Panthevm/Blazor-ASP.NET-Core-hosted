0b6363e9efe8495a17db7eeabf088c35fcc7b91a
Panthevm <43318093+Panthevm@users.noreply.github.com>
2019-05-09 12:32:31 +0300

    Test

--- Dekanat.Client/Startup.cs
+++ Dekanat.Client/Startup.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dekanat.Client {
+
     public class Startup {
         public void ConfigureServices(IServiceCollection services) {
         }
-
+        int test;
         public void Configure(IComponentsApplicationBuilder app) {
             app.AddComponent<App>("app");
         }
