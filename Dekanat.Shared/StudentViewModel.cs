45e7c823735bf99c9a40306c44181902b3f2774f
Panthevm <43318093+Panthevm@users.noreply.github.com>
2019-05-11 23:07:26 +0300

    Add Student data and Student page

---
+++ Dekanat.Shared/StudentViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Dekanat.Shared {
+    public class StudentViewModel {
+        [Display(Name = "Имя")]
+        public string FirsName { get; set; }
+
+        [Display(Name = "Отчество")]
+        public string MiddleName { get; set; }
+
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Факультет")]
+        public string Faculty { get; set; }
+
+        [Display(Name = "Кафедра")]
+        public string Pulpit { get; set; }
+
+        [Display(Name = "Направление подготовки")]
+        public string TrainingDirection { get; set; }
+
+        [Display(Name = "Дата поступления")]
+        public DateTime ReceiptDate { get; set; }
+
+        [Display(Name = "Год рождения")]
+        public DateTime YearOfBirth { get; set; }
+
+        [Display(Name = "Форма обучения")]
+        public string FormOfStudy { get; set; }
+
+        [Display(Name = "Адрес проживания")]
+        public string Address { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        public string PhoneNumber { get; set; }
+    }
+}
